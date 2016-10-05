0bda7e41b1cf52bb8fd61b99bdf423a7f83a782c
Sathya <v.sathyamanoj@gmail.com>
2016-10-05 16:27:37 +0530

    post message to server

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,14 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Request-Headers","Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
+    next();
+})
+
 app.post('/api/message',function(req,res){
     console.log(req.body);
     res.status(200);
