2956666fae7f0d3a4ecd6728320f9a361b52c625
Sathya <v.sathyamanoj@gmail.com>
2016-10-06 22:06:16 +0530

    configuring satellizer for authentication

--- front-end/src/app/auth/auth.controller.js
+++ front-end/src/app/auth/auth.controller.js
@@ -1,3 +1,13 @@
 export class AuthController{
 
+    constructor($auth){
+        'ngInject';
+
+        this.$auth = $auth;
+
+    }
+    register(){
+        console.log("test");
+        this.$auth.signup({email:'test@test.com'});
+    }
 }
