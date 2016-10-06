fd3e8dbc64da18e8a2b9d029c46343756c05d8be
Sathya <v.sathyamanoj@gmail.com>
2016-10-06 12:32:36 +0530

    display all our messages on view using angular

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -1,13 +1,30 @@
 export class MainController {
-  constructor ($http) {
+  constructor ($http,$timeout) {
     'ngInject';
       this.$http = $http;
+      this.$timeout = $timeout;
+      this.getMessages(); // on page load see all messages in console
+
 
   }
+
     postMessage(){
-       this.$http.post('http://localhost:5000/api/message', {msg:this.message});
+        var vm = this;
+        this.$http.post('http://localhost:5000/api/message', {msg:this.message})
+
+        this.message = '';
+         this.$timeout(function() {
+          vm.getMessages();
+        }, 1000);
     }
 
+    getMessages(){
+        var vm = this;
+        this.$http.get('http://localhost:5000/api/message').then(function(result){
+         console.log(result);
+            vm.messages = result.data;
+        });
+    }
 
 
 
