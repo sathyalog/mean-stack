ed9df2bf69139b1fd0c38faa9676090f2b3a4116
sathyalog <v.sathyamanoj@gmail.com>
2016-10-25 16:52:18 +0530

    login and logout functionalities added

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -24,6 +24,28 @@ module.exports = {
                 res.status(200).send({token: createToken(result)});
             })
         });
+    },
+    login: function (req, res) {
+        User.findOne({
+            email: req.body.email
+        }, function (err, user) {
+
+            if (!user)
+                return res.status(401).send({
+                    message: 'Email or Password invalid'
+                });
+
+            if (req.body.pwd == user.pwd) {
+                console.log(req.body, user.pwd)
+                res.send({
+                    token: createToken(user)
+                });
+            } else {
+                return res.status(401).send({
+                    message: 'Invalid email and/or password'
+                });
+            }
+        });
     }
 }
 
